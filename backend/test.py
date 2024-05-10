bc1b938188b693def20a66307a3997f0b99c43b3
Nkwe <nkabo074@gmail.com>
2024-05-10 20:06:04 +0100

    another massive commit

--- backend/test.py
+++ backend/test.py
@@ -2,13 +2,13 @@ from image_processing import bicubic_interpolation
 from file_processing import load_image, save_image
 
 # Load the original image
-original_image = load_image("./backgrond01.jpg")
+original_image = load_image("./tanjiro.jpg")
 
 # Define the scaling factor
-scaling_factor = 2
+scaling_factor = 4
 
 # Perform bicubic interpolation for upscaling
 upscaled_image = bicubic_interpolation(original_image, scaling_factor)
 
 # Save or display the upscaled image
-save_image(upscaled_image, "output.jpg")
+save_image(upscaled_image, "output2.jpg")
